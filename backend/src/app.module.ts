59d86b77c7073ebd59bdd898fa7f47550db90b2a
mitrofanov <83586607+exodie@users.noreply.github.com>
2023-11-12 01:49:10 +0400

    add backend-database module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './services/auth/auth.module';
 import { EmailModule } from './services/email/email.module';
+import { DatabaseModule } from './services/database/database.module';
 
 @Module({
-  imports: [AuthModule, EmailModule],
+  imports: [AuthModule, EmailModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
