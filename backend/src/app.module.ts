75df69aeab3e94251b5107094b3eb557ced2a222
mitrofanov <83586607+exodie@users.noreply.github.com>
2023-11-15 12:45:23 +0400

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,19 @@ import { AppService } from './app.service';
 import { AuthModule } from './services/auth/auth.module';
 import { EmailModule } from './services/email/email.module';
 import { DatabaseModule } from './services/database/database.module';
+import { DriversModule } from './entities/drivers/drivers.module';
+import { CustomersService } from './entities/customers/customers.service';
+import { CustomersModule } from './entities/customers/customers.module';
 
 @Module({
-  imports: [AuthModule, EmailModule, DatabaseModule],
+  imports: [
+    AuthModule,
+    EmailModule,
+    DatabaseModule,
+    DriversModule,
+    CustomersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CustomersService],
 })
 export class AppModule {}
