1cb572f9b278e2f2fd8a916a22cfba8c03ac9b27
mitrofanov <83586607+exodie@users.noreply.github.com>
2023-11-15 12:47:57 +0400

    refactor services-controller
    Add ApiTags for Swagger

--- backend/src/services/email/email.controller.ts
+++ backend/src/services/email/email.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+import { EmailService } from './email.service';
+
+@ApiTags('email')
 @Controller('email')
-export class EmailController {}
+export class EmailController {
+  constructor(private readonly emailService: EmailService) {}
+}
